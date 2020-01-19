b2019a62aed81318a236cd0221962613b7088b85
aluc <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2020-01-20 00:26:51 +0900

    fix: add getInitialProps in Index.tsx
    - A page that relies on publicRuntimeConfig must use getInitialProps to opt-out of Automatic Static Optimization.
    - https://nextjs.org/docs/api-reference/next.config.js/runtime-configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import getConfig from 'next/config'
 import { CSSProperties } from 'react'
+import { NextPage } from 'next'
 const { publicRuntimeConfig } = getConfig()
 
 const styleGreenLight: CSSProperties = {
@@ -11,19 +12,23 @@ const styleGreenLight: CSSProperties = {
   marginRight: 10,
 }
 
-export default () => {
-  return (
-    <>
-      <div style={{fontSize: 40}}>
-        <span style={styleGreenLight}/>
-        <span>Anonymouslack server in on!!</span>
-      </div>
-      <div style={{height: 20}}/>
-      <div style={{fontSize: 20}}>publicRuntimeConfig:</div>
-      <div style={{height: 10}}/>
-      <pre>
-        { JSON.stringify(publicRuntimeConfig, null, 2) }
-      </pre>
-    </>
-  )
+const Index: NextPage = () => (
+  <>
+    <div style={{fontSize: 40}}>
+      <span style={styleGreenLight}/>
+      <span>Anonymouslack server in on!!</span>
+    </div>
+    <div style={{height: 20}}/>
+    <div style={{fontSize: 20}}>publicRuntimeConfig:</div>
+    <div style={{height: 10}}/>
+    <pre>
+      { JSON.stringify(publicRuntimeConfig, null, 2) }
+    </pre>
+  </>
+)
+
+Index.getInitialProps = async () => {
+  return {}
 }
+
+export default Index
